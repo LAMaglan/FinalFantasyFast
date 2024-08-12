15cbffdbe6d6a633de2c9c8fa462d3c8db0986fe
Luigi Maglanoc <luigi_maglanoc@hotmail.com>
2024-08-12 09:29:33 +0200

    store and retrieve data in local database

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Run with poetry:
 poetry shell
 uvicorn app.main:app --reload
 ```
+
+Retrieves data from external Final fantasy api
+https://www.moogleapi.com/
+
+With default settings, you can test out the API through swagger docs:
+http://127.0.0.1:8000/docs/
+
+The `characters` and `monsters` routers fetches data from the above link,
+and stores them in a sqlite3 database (sql_app.db)
--- app/routers/characters.py
+++ app/routers/characters.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 import httpx
+from ..database import get_db
+from ..crud import create_character, get_characters
 
 router = APIRouter()
 
 @router.get("/characters")
-async def get_characters():
+async def fetch_and_store_characters(db: Session = Depends(get_db)):
     async with httpx.AsyncClient() as client:
         response = await client.get('https://www.moogleapi.com/api/v1/characters')
         response.raise_for_status()
-        return response.json()
+        characters = response.json()
+        for character in characters:
+            if create_character(db, character) is None:
+                return {"error": "Cannot insert or update character."}
+        return characters
+
+@router.get("/stored-characters")
+def read_characters(db: Session = Depends(get_db)):
+    return get_characters(db)
