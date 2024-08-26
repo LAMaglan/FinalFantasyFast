c244000232e35daf3c770db090e149c57ed3a4a8
Luigi Maglanoc <luigi_maglanoc@hotmail.com>
2024-08-26 12:41:48 +0200

    display games in frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Tabs, Tab, Box } from '@mui/material';
+import Game from './components/Game';
 import Character from './components/Character';
 import Monster from './components/Monster';
 import './App.css';
@@ -19,12 +20,14 @@ const App = () => {
             <main>
                 <Box sx={{ width: '100%' }}>
                     <Tabs value={value} onChange={handleChange} centered>
+                        <Tab label="Games" />
                         <Tab label="Characters" />
                         <Tab label="Monsters" />
                     </Tabs>
                     <div className="tab-content">
-                        {value === 0 && <Character />}
-                        {value === 1 && <Monster />}
+                        {value === 0 && <Game />}
+                        {value === 1 && <Character />}
+                        {value === 2 && <Monster />}
                     </div>
                 </Box>
             </main>
